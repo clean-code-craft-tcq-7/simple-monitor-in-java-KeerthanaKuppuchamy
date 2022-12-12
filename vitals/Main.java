8c94a1f5a62d78f9ff1ccdb19d5c8288024b2b2a
Keerthana Kuppuchamy (SX/EAB1) <feu1cob@bosch.com>
2022-12-12 20:05:18 +0530

    Code change

--- vitals/Main.java
+++ vitals/Main.java
@@ -1,23 +1,36 @@
 package vitals;
 
 public class Main {
-    static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
-            System.out.println("Temperature is out of range!");
-            return false;
-        } else if(soc < 20 || soc > 80) {
-            System.out.println("State of Charge is out of range!");
-            return false;
-        } else if(chargeRate > 0.8) {
-            System.out.println("Charge Rate is out of range!");
-            return false;
-        }
-        return true;
-    }
 
-    public static void main(String[] args) {
-        assert(batteryIsOk(25, 70, 0.7f) == true);
-        assert(batteryIsOk(50, 85, 0.0f) == false);
-        System.out.println("Some more tests needed");
-    }
+	static boolean isThresholdBreach(int startThreshold, int endThreshold, float currentValue, String errorMessage) {
+		if (currentValue < startThreshold || currentValue > endThreshold) {
+			System.out.println(errorMessage);
+			return false;
+		}
+		return true;
+	}
+
+	static boolean isChargeRateNormal(double limit, float currentValue, String errorMessage) {
+		if (currentValue > limit) {
+			System.out.println(errorMessage);
+			return false;
+		}
+		return true;
+	}
+
+	static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
+		boolean isTemperatureNormal = isThresholdBreach(0, 45, temperature, "Temperature is out of range!");
+		boolean isSocNormal = isThresholdBreach(20, 80, soc, "State of Charge is out of range!");
+		boolean isChargeRateNormal = isChargeRateNormal(0.8, chargeRate, "Charge Rate is out of range!");
+		return isTemperatureNormal && isSocNormal && isChargeRateNormal;
+	}
+
+	public static void main(String[] args) {
+		assert (isThresholdBreach(0, 45, 20, "") == true);
+		assert (isThresholdBreach(20, 80, 20, "") == true);
+		assert (isChargeRateNormal(0.8, 0.4f, "") == true);
+		assert (batteryIsOk(25, 70, 0.7f) == true);
+		assert (batteryIsOk(50, 85, 0.0f) == false);
+		System.out.println("Some more tests needed");
+	}
 }
