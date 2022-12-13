20e95a7bad4fc86be8dd417160226c4c9611b112
Keerthana Kuppuchamy (SX/EAB1) <feu1cob@bosch.com>
2022-12-13 13:38:31 +0530

    Code change

--- vitals/Main.java
+++ vitals/Main.java
@@ -2,35 +2,45 @@ package vitals;
 
 public class Main {
 
-	static boolean isThresholdBreach(int startThreshold, int endThreshold, float currentValue, String errorMessage) {
+	static boolean isThresholdBreach(float startThreshold, float endThreshold, float currentValue,
+			String errorMessage) {
 		if (currentValue < startThreshold || currentValue > endThreshold) {
-			System.out.println(errorMessage);
+			System.out.println(errorMessage + " is out of range!");
 			return false;
 		}
 		return true;
 	}
 
-	static boolean isChargeRateNormal(double limit, float currentValue, String errorMessage) {
+	static boolean isChargeRateNormal(float limit, float currentValue, String errorMessage) {
 		if (currentValue > limit) {
-			System.out.println(errorMessage);
+			System.out.println(errorMessage + " is out of range!");
 			return false;
 		}
 		return true;
 	}
 
 	static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-		boolean isTemperatureNormal = isThresholdBreach(0, 45, temperature, "Temperature is out of range!");
-		boolean isSocNormal = isThresholdBreach(20, 80, soc, "State of Charge is out of range!");
-		boolean isChargeRateNormal = isChargeRateNormal(0.8, chargeRate, "Charge Rate is out of range!");
+		boolean isTemperatureNormal = isThresholdBreach(0, 45, temperature, "Temperature");
+		boolean isSocNormal = isThresholdBreach(20, 80, soc, "State of Charge");
+		boolean isChargeRateNormal = isChargeRateNormal(0.8f, chargeRate, "Charge Rate");
 		return isTemperatureNormal && isSocNormal && isChargeRateNormal;
 	}
 
 	public static void main(String[] args) {
-		assert (isThresholdBreach(0, 45, 20, "") == true);
-		assert (isThresholdBreach(20, 80, 20, "") == true);
-		assert (isChargeRateNormal(0.8, 0.4f, "") == true);
+		assert (isThresholdBreach(0, 45, 20, "Temperature") == true);
+		assert (isThresholdBreach(0, 45, 0, "Temperature") == true);
+		assert (isThresholdBreach(0, 45, 45, "Temperature") == true);
+
+		assert (isThresholdBreach(20, 80, 20, "State of Charge") == true);
+		assert (isThresholdBreach(20, 80, 21, "State of Charge") == true);
+		assert (isThresholdBreach(20, 80, 81, "State of Charge") == false);
+
+		assert (isChargeRateNormal(0.8f, 0.4f, "Charge Rate") == true);
+		assert (isChargeRateNormal(0.8f, 0.9f, "Charge Rate") == false);
+
 		assert (batteryIsOk(25, 70, 0.7f) == true);
 		assert (batteryIsOk(50, 85, 0.0f) == false);
+
 		System.out.println("Some more tests needed");
 	}
 }
