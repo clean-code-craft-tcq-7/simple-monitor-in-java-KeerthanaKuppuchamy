1cc85d55c295958532bc9f0feae2da1437f70cf1
Keerthana Kuppuchamy (SX/EAB1) <feu1cob@bosch.com>
2022-12-15 15:54:21 +0530

    Code review changes

--- vitals/BatteryManagement.java
+++ vitals/BatteryManagement.java
@@ -1,6 +1,6 @@
 package vitals;
 
-public class Main {
+public class BatteryManagement {
 
 	static boolean isThresholdBreach(float startThreshold, float endThreshold, float currentValue,
 			String errorMessage) {
@@ -24,23 +24,5 @@ public class Main {
 		boolean isSocNormal = isThresholdBreach(20, 80, soc, "State of Charge");
 		boolean isChargeRateNormal = isChargeRateNormal(0.8f, chargeRate, "Charge Rate");
 		return isTemperatureNormal && isSocNormal && isChargeRateNormal;
-	}
-
-	public static void main(String[] args) {
-		assert (isThresholdBreach(0, 45, 20, "Temperature") == true);
-		assert (isThresholdBreach(0, 45, 0, "Temperature") == true);
-		assert (isThresholdBreach(0, 45, 45, "Temperature") == true);
-
-		assert (isThresholdBreach(20, 80, 20, "State of Charge") == true);
-		assert (isThresholdBreach(20, 80, 21, "State of Charge") == true);
-		assert (isThresholdBreach(20, 80, 81, "State of Charge") == false);
-
-		assert (isChargeRateNormal(0.8f, 0.4f, "Charge Rate") == true);
-		assert (isChargeRateNormal(0.8f, 0.9f, "Charge Rate") == false);
-
-		assert (batteryIsOk(25, 70, 0.7f) == true);
-		assert (batteryIsOk(50, 85, 0.0f) == false);
-
-		System.out.println("Some more tests needed");
 	}
 }
