c938059e715136d521e1f2ffc9caa4791027fc16
Keerthana Kuppuchamy (SX/EAB1) <feu1cob@bosch.com>
2023-01-03 19:38:08 +0530

    Code changes

--- vitals/Constants.java
+++ vitals/Constants.java
@@ -13,9 +13,9 @@ public class Constants {
 	public static final String SOC_ERROR_ENGLISH = "State of Charge is out of range!";
 	public static final String CHARGE_RATE_ERROR_ENGLISH = "Charge Rate is out of range!";
 
-	public static final String TEMPERATURE_ERROR_GERMAN = "Temperatur auﬂerhalb des Bereichs!";
-	public static final String CHARGE_RATE_ERROR_GERMAN = "Die Laderate liegt auﬂerhalb des Bereichs!";
-	public static final String SOC_ERROR_GERMAN = "Ladezustand auﬂerhalb des Bereichs!";
+	public static final String TEMPERATURE_ERROR_GERMAN = "Temperatur auberhalb des Bereichs!";
+	public static final String CHARGE_RATE_ERROR_GERMAN = "Die Laderate liegt auberhalb des Bereichs!";
+	public static final String SOC_ERROR_GERMAN = "Ladezustand auberhalb des Bereichs!";
 
 	public static final String TEMPERATURE_LOW_WARN_ENGLISH = "Temperature is nearing lower threshold limit";
 	public static final String TEMPERATURE_HIGH_WARN_ENGLISH = "Temperature is nearing higher threshold limit";
@@ -24,10 +24,10 @@ public class Constants {
 	public static final String CHARGE_RATE_LOW_WARN_ENGLISH = "Charge Rate is nearing lower threshold limit";
 	public static final String CHARGE_RATE_HIGH_WARN_ENGLISH = "Charge Rate is nearing higher threshold limit";
 
-	public static final String TEMPERATURE_LOW_WARN_GERMAN = "Die Temperatur n‰hert sich dem unteren Schwellenwert";
-	public static final String TEMPERATURE_HIGH_WARN_GERMAN = "Die Temperatur n‰hert sich dem oberen Grenzwert";
-	public static final String SOC_LOW_WARN_GERMAN = "Der Ladezustand n‰hert sich dem unteren Schwellenwert";
-	public static final String SOC_HIGH_WARN_GERMAN = "Der Ladezustand n‰hert sich dem oberen Schwellenwert";
-	public static final String CHARGE_RATE_LOW_WARN_GERMAN = "Die Laderate n‰hert sich dem unteren Grenzwert";
-	public static final String CHARGE_RATE_HIGH_WARN_GERMAN = "Die Laderate n‰hert sich dem oberen Grenzwert";
+	public static final String TEMPERATURE_LOW_WARN_GERMAN = "Die Temperatur nahert sich dem unteren Schwellenwert";
+	public static final String TEMPERATURE_HIGH_WARN_GERMAN = "Die Temperatur nahert sich dem oberen Grenzwert";
+	public static final String SOC_LOW_WARN_GERMAN = "Der Ladezustand nahert sich dem unteren Schwellenwert";
+	public static final String SOC_HIGH_WARN_GERMAN = "Der Ladezustand nahert sich dem oberen Schwellenwert";
+	public static final String CHARGE_RATE_LOW_WARN_GERMAN = "Die Laderate nahert sich dem unteren Grenzwert";
+	public static final String CHARGE_RATE_HIGH_WARN_GERMAN = "Die Laderate nahert sich dem oberen Grenzwert";
 }
